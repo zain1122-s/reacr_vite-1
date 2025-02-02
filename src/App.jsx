08c5a7f4e4fedc7da17cd2637486282291c3f256
shahzain alam <shahzaina354@gmail.com>
2025-02-02 18:04:26 +0000

    chng

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,74 @@ function App() {
     <>
       <div className="container">
         <H1 heading={"The modern landing  page  for react developer"} />
-        <Card para={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat itaque quam ab  consequatur quisquam sae "} />
-        <Card para={" sequi fugit adipisci doloribus? Nobis sequi quo impedit! Omnis  perspiciatis sit beatae."}/>
+        <Card
+          para={
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat itaque quam ab  consequatur quisquam sae "
+          }
+        />
+        <Card
+          para={
+            " sequi fugit adipisci doloribus? Nobis sequi quo impedit! Omnis  perspiciatis sit beatae."
+          }
+        />
         <Button />
-
       </div>
       <div className="maindev">
         <div className="body">
           <H1 heading={"Trusted by over 8000+ customer"} />
         </div>
         <div className="pra">
-        <Card  para={"lorem shah Lorem ipsum dolor, sit amet consectetur adipisci doloribus? Nobis sequi quo impedit! adipisicing elit. Fugiat itaque quam ab "} />
+          <Card
+            para={
+              "lorem shah Lorem ipsum dolor, sit amet consectetur adipisci doloribus? Nobis sequi quo impedit! adipisicing elit. Fugiat itaque quam ab "
+            }
+          />
         </div>
       </div>
       <div className="data">
         <div className="f1">
-          <H1 heading={"#5000k+"}/>
+          <H1 heading={"$5000k+"} />
+          <Card para={"revenur"} />
         </div>
         <div className="f2">
-          <H1 heading={"10+"}/>
+          <H1 heading={"10+"} />
+          <Card para={"products"} />
         </div>
         <div className="f3">
-          <H1 heading={"150+"}/>
+          <H1 heading={"150+"} />
+          <Card para={"component"} />
         </div>
         <div className="f4">
-          <H1 heading={"10+"}/>
+          <H1 heading={"10+"} />
+          <Card para={"employes"} />
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="ma1">
+          <H1 heading={"feature"} />
+        </div>
+        <div className="ma2">
+        <H1 heading={"your title here"} />
+        </div>
+        <div className="ma3">
+          <Card para="rem shah Lorem ipsum dolor, sit amet consectetur adipisci doloribus? Nobis sequi quo impedit! adipisicing elit. Fugiat itaque quam a"/>
+        </div>
+        
+        <div className="box">
+          <div className="box1">
+            <H1 heading="your title"/>
+            <Card para="rem shah Lorem ipsum dolor, sit amet"/>
+          </div>
+          <div className="box2">
+            <H1  heading="your title"/>
+            <Card para="rem shah Lorem ipsum dolor, sit amet"/>
+          </div>
+          <div className="box3">
+            <H1 heading="your title"/>
+            <Card para="rem shah Lorem ipsum dolor, sit amet"/>
+          </div>
+          
         </div>
       </div>
     </>
